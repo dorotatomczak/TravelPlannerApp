dbc46bd35938268fdb557d33d1af967204d797d7
magasol <magdalena.j.solecka@gmail.com>
2019-07-05 23:49:04 +0200

    13: created first sign in and sign up form user interfaces

--- App/app/src/main/java/com/github/travelplannerapp/signin/SignInContract.kt
+++ App/app/src/main/java/com/github/travelplannerapp/signin/SignInContract.kt
@@ -3,9 +3,11 @@ package com.github.travelplannerapp.signin
 interface SignInContract {
     interface View {
         fun showTravels()
+        fun showSignUp()
     }
 
     interface Presenter {
         fun signIn()
+        fun signUp()
     }
 }
--- App/app/src/main/java/com/github/travelplannerapp/signin/SignInPresenter.kt
+++ App/app/src/main/java/com/github/travelplannerapp/signin/SignInPresenter.kt
@@ -3,6 +3,9 @@ package com.github.travelplannerapp.signin
 import com.github.travelplannerapp.BasePresenter
 
 class SignInPresenter(view: SignInContract.View) : BasePresenter<SignInContract.View>(view), SignInContract.Presenter {
+    override fun signUp() {
+        view.showSignUp()
+    }
 
     override fun signIn() {
         //example of the view and presenter flow
