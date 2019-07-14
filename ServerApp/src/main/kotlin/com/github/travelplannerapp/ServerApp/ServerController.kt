9056cda4ab2c8d6ed2916ad0c86e48cc5cada5f6
ani119 <karolina_makuch_email@wp.pl>
2019-07-14 21:24:57 +0200

    Add simple HTTP Request - GET

--- ServerApp/src/main/kotlin/com/github/travelplannerapp/ServerApp/ServerController.kt
+++ ServerApp/src/main/kotlin/com/github/travelplannerapp/ServerApp/ServerController.kt
@@ -1,6 +1,7 @@
 package com.github.travelplannerapp.serverapp
 
 
+import com.github.travelplannerapp.ServerApp.HTTPCharger
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -14,5 +15,16 @@ class ServerController {
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
             Greeting(counter.incrementAndGet(), "Hello, $name")
+
+    @GetMapping("/HERE")
+    fun getDataFromHERE()
+    {
+        println("Downloading data from HERE")
+        val charger=HTTPCharger();
+        charger.testURLs();
+        charger.us.printUserLocation();
+
+    }
+
 
 }
