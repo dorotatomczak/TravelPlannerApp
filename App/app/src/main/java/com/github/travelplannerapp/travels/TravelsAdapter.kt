2e769077a2260439918aa956bc11f015564fb645
Anna Malizjusz <a.malizjusz@vp.pl>
2019-07-15 22:26:15 +0200

    Update TravelsAdapter.kt
    Delete commented code

--- App/app/src/main/java/com/github/travelplannerapp/travels/TravelsAdapter.kt
+++ App/app/src/main/java/com/github/travelplannerapp/travels/TravelsAdapter.kt
@@ -39,20 +39,4 @@ class TravelsAdapter (val presenter: TravelsContract.Presenter):RecyclerView.Ada
             textViewItemTravelName.text = name
         }
     }
-    /*@BindingAdapter({"entries", "layout"})
-public static <T> void setEntries(ViewGroup viewGroup,
-                                  List<T> entries, int layoutId) {
-    viewGroup.removeAllViews();
-    if (entries != null) {
-        LayoutInflater inflater = (LayoutInflater)
-            viewGroup.getContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        for (int i = 0; i < entries.size(); i++) {
-            T entry = entries.get(i);
-            ViewDataBinding binding = DataBindingUtil
-                .inflate(inflater, layoutId, viewGroup, true);
-            binding.setVariable(BR.data, entry);
-        }
-    }
-}*/
 }
