99c49f09606f81f007a04687276e38de00822bc8
magasol <magdalena.j.solecka@gmail.com>
2019-07-24 14:52:55 +0200

    #9: Kotlin back at previously used version, added application.properties

--- ServerApp/build.gradle.kts
+++ ServerApp/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.1.6.RELEASE"
 	id("io.spring.dependency-management") version "1.0.7.RELEASE"
-	kotlin("jvm") version "1.3.41"
-	kotlin("plugin.spring") version "1.3.41"
+	kotlin("jvm") version "1.2.71"
+	kotlin("plugin.spring") version "1.2.71"
 }
 
 group = "com.github.travelplannerapp"
