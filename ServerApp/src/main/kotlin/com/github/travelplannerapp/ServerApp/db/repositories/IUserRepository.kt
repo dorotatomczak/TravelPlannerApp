6ebee85a382eae9400a045adeecb7e17c0d218ab
magasol <32590993+magasol@users.noreply.github.com>
2019-08-01 18:41:15 +0200

    #26: Added getting user by email (#32)
    resolves #26: Added getting user by email

--- ServerApp/src/main/kotlin/com/github/travelplannerapp/ServerApp/db/repositories/IUserRepository.kt
+++ ServerApp/src/main/kotlin/com/github/travelplannerapp/ServerApp/db/repositories/IUserRepository.kt
@@ -2,4 +2,6 @@ package com.github.travelplannerapp.ServerApp.db.repositories
 
 import com.github.travelplannerapp.ServerApp.db.dao.User
 
-interface IUserRepository: IRepository<User>
+interface IUserRepository: IRepository<User> {
+    fun getUserByEmail(email: String): User?
+}
