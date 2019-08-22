e10663737df70248af951c88b8c4115e325d28a0
qwed245 <a.malizjusz@vp.pl>
2019-08-22 21:00:23 +0200

    #55: changed hashing algorithm

--- App/app/src/main/java/com/github/travelplannerapp/utils/PasswordUtils.kt
+++ App/app/src/main/java/com/github/travelplannerapp/utils/PasswordUtils.kt
@@ -1,8 +1,12 @@
 package com.github.travelplannerapp.utils
 
+import java.security.MessageDigest
+
 class PasswordUtils {
 
     fun hashPassword(password: String): String? {
-        return password.hashCode().toString()
+        return MessageDigest.getInstance("SHA-512")
+                .digest(password.toByteArray())
+                .joinToString("", limit = 40) { Integer.toHexString(it.toInt()) }
     }
 }
