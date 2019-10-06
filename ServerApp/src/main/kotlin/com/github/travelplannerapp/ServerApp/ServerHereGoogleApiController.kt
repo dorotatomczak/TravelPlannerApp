0a59d12466aca6ca8488eb10d9811fab6b77edda
qwed245 <a.malizjusz@vp.pl>
2019-10-06 18:13:02 +0200

    #105: Added Google API

--- ServerApp/src/main/kotlin/com/github/travelplannerapp/ServerApp/ServerHereGoogleApiController.kt
+++ ServerApp/src/main/kotlin/com/github/travelplannerapp/ServerApp/ServerHereGoogleApiController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 
 
 @RestController
-class ServerHereController {
+class ServerHereGoogleApiController {
 
     @Autowired
     lateinit var userManagement: UserManagement
