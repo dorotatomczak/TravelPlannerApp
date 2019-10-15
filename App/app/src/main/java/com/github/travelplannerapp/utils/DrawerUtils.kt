abe36078eba2f6acf7ccb173305814845a7d5b74
dorotatomczak <dorotatomczak@carfax.com>
2019-10-15 14:24:54 +0200

    #59: Add profile info to drawer, fix bug with place categories

--- App/app/src/main/java/com/github/travelplannerapp/utils/DrawerUtils.kt
+++ App/app/src/main/java/com/github/travelplannerapp/utils/DrawerUtils.kt
@@ -7,23 +7,38 @@ import androidx.appcompat.widget.Toolbar
 import com.github.travelplannerapp.R
 import com.github.travelplannerapp.searchfriend.SearchFriendActivity
 import com.github.travelplannerapp.signin.SignInActivity
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 object DrawerUtils {
 
     fun getDrawer(activity: Activity, toolbar: Toolbar) {
 
+        val headerResult = AccountHeaderBuilder()
+                .withActivity(activity)
+                .addProfiles(
+                        ProfileDrawerItem()
+                                .withEmail(SharedPreferencesUtils.getEmail())
+                                .withIcon(activity.getDrawable(R.drawable.profile_picture))
+                )
+                .withSelectionListEnabled(false)
+
+                .build()
+
+
+        val drawerItemSearchFriend = PrimaryDrawerItem().withIdentifier(Menu.SEARCH_FRIEND.ordinal.toLong())
+                .withName(Menu.SEARCH_FRIEND.color).withIcon(Menu.SEARCH_FRIEND.icon)
         val drawerItemSettings = PrimaryDrawerItem().withIdentifier(Menu.SETTINGS.ordinal.toLong())
                 .withName(Menu.SETTINGS.color).withIcon(Menu.SETTINGS.icon)
         val drawerItemSignOut = PrimaryDrawerItem().withIdentifier(Menu.SIGN_OUT.ordinal.toLong())
                 .withName(Menu.SIGN_OUT.color).withIcon(Menu.SIGN_OUT.icon)
-        val drawerItemSearchFriend = PrimaryDrawerItem().withIdentifier(Menu.SEARCH_FRIEND.ordinal.toLong())
-                .withName(Menu.SEARCH_FRIEND.color).withIcon(Menu.SEARCH_FRIEND.icon)
 
         DrawerBuilder()
+                .withAccountHeader(headerResult)
                 .withActivity(activity)
                 .withToolbar(toolbar)
                 .withActionBarDrawerToggle(true)
@@ -31,14 +46,13 @@ object DrawerUtils {
                 .withCloseOnClick(true)
                 .withSelectedItem(-1)
                 .addDrawerItems(
+                        drawerItemSearchFriend,
                         drawerItemSettings,
-                        drawerItemSignOut,
-                        drawerItemSearchFriend
-
+                        drawerItemSignOut
                 )
                 .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
                     override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*>): Boolean {
-                        view?.let { Menu.getItem(position).onClick(it) }
+                        view?.let { Menu.getItem(position-1).onClick(it) }
                         return true
                     }
                 })
@@ -46,6 +60,12 @@ object DrawerUtils {
     }
 
     enum class Menu(val color: Int, val icon: Int) : OnMenuItemClickListener {
+        SEARCH_FRIEND(R.string.my_friends, R.drawable.ic_user) {
+            override fun onClick(view: View) {
+                val intent = Intent(view.context, SearchFriendActivity::class.java)
+                view.context.startActivity(intent)
+            }
+        },
         SETTINGS(R.string.menu_settings, R.drawable.ic_settings) {
             override fun onClick(view: View) {
                 //TODO Add SettingsActivity
@@ -57,12 +77,6 @@ object DrawerUtils {
 
                 val intent = Intent(view.context, SignInActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                view.context.startActivity(intent)
-            }
-        },
-        SEARCH_FRIEND(R.string.my_friends, R.drawable.ic_user) {
-            override fun onClick(view: View) {
-                val intent = Intent(view.context, SearchFriendActivity::class.java)
                 view.context.startActivity(intent)
             }
         };
